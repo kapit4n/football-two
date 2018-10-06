77387608f24f644864767135c4ba2e67893a76e8
lartl <luis.arce22@gmail.com>
2018-10-06 09:02:46 -0400

    Make standings a component

--- src/app/common/standing-table/standing-table.component.ts
+++ src/app/common/standing-table/standing-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-standing-table',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class StandingTableComponent implements OnInit {
 
   constructor() { }
+  @Input()
+  standings: any[];
 
   ngOnInit() {
   }
